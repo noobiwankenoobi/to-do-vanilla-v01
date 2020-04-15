c5d1587048bda613ce82d80fb2c7e5d11cd1fde2
Ross Hazelett <rhhazelett@gmail.com>
2020-04-15 14:58:22 -0400

    added basic css to look nice, started js

---
+++ app.js
@@ -0,0 +1,17 @@
+// SELECTORS
+
+const todoInput = document.querySelector('todo-input');
+const todoButton = document.querySelector('todo-button');
+const todoList = document.querySelector('todo-list');
+
+
+// EVENT LISTENERS
+
+todoButton.addEventListener('click', addTodo);
+
+
+// FUNCTIONS
+
+function addTodo() {
+
+}
